b70420fc34846a0e15ad1f520ae00f540b49abc9
XalaMental <s79j2jb2c2@privaterelay.appleid.com>
2026-08-20 20:21:04 -0400

    - New file setting up the addon's namespace.

---
+++ Bootstrap.lua
@@ -0,0 +1,11 @@
+-- Bootstrap.lua
+-- Xal's Routes Data
+local addonName, addonTable = ...
+
+-- Main namespace definition - add a line here for every module file as it's built
+addonTable.BrandStyle    = {}
+addonTable.Import        = {}
+addonTable.WhatsNew      = {}
+addonTable.Engine        = {}
+addonTable.SettingsPanel = {}
+addonTable.ChatCommands  = {}
